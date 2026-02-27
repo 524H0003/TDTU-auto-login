baf0e28e6b36c3afda612ffd1758935606fa0736
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-02-27 00:00:38 +0000

    chore(lint): modified ignore paths

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,7 +9,7 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    "build/**",
+    "dist/**",
   ]),
   {
     plugins: {
