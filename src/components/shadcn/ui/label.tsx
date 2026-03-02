688e6a08967f9973dfa2f8d82e3b3028db261c83
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-02 08:17:28 +0000

    chore: update files

--- src/components/shadcn/ui/label.tsx
+++ src/components/shadcn/ui/label.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from "react"
 import { Label as LabelPrimitive } from "radix-ui"
 
@@ -21,26 +20,3 @@ function Label({
 }
 
 export { Label }
-=======
-import { cn } from "@/lib/shadcn/utils";
-import { Label as LabelPrimitive } from "radix-ui";
-import * as React from "react";
-
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
-  return (
-    <LabelPrimitive.Root
-      data-slot="label"
-      className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-export { Label };
->>>>>>> ceae5757d5ce8ef8dc44550c3b726f8b399b8bfd
--- src/components/shadcn/ui/separator.tsx
+++ src/components/shadcn/ui/separator.tsx
@@ -1,8 +1,9 @@
-"use client";
+"use client"
 
-import { cn } from "@/lib/shadcn/utils";
-import { Separator as SeparatorPrimitive } from "radix-ui";
-import * as React from "react";
+import * as React from "react"
+import { Separator as SeparatorPrimitive } from "radix-ui"
+
+import { cn } from "@/lib/shadcn/utils"
 
 function Separator({
   className,
@@ -17,11 +18,11 @@ function Separator({
       orientation={orientation}
       className={cn(
         "bg-border shrink-0 data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
-        className,
+        className
       )}
       {...props}
     />
-  );
+  )
 }
 
-export { Separator };
+export { Separator }
