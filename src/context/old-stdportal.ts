40d86a72e1f295d93e903745c012592cf5882618
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-02 14:55:55 +0000

    feat: add postFunc, conditionFunc, functional url for old-stdportal

--- src/context/old-stdportal.ts
+++ src/context/old-stdportal.ts
@@ -1,7 +1,26 @@
 import { execute } from ".";
 
-execute({
+execute<{ url: string }>({
   usernameField: "user",
   passwordField: "pass",
-  url: "https://old-stdportal.tdtu.edu.vn/Login/SignIn",
+  url: () =>
+    "https://old-stdportal.tdtu.edu.vn/Login/SignIn?ReturnURL=" +
+    window.location.origin +
+    window.location.pathname,
+  postFunc: async ({ url }) => {
+    window.location.href = url;
+  },
+  conditionFunc: async () => {
+    const response = await fetch(
+      "https://old-stdportal.tdtu.edu.vn/Alert/ThongBaoNotification",
+      { method: "Post" },
+    );
+
+    if (!response.ok) return false;
+    else {
+      const data = await response.text();
+
+      return data !== "[]";
+    }
+  },
 });
