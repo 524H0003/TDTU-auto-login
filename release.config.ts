0a7ab2cc372c745f2e1adf6ffad5ff0fc5c6484c
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-02 08:43:13 +0000

    chore: modify repository's url

--- release.config.ts
+++ release.config.ts
@@ -2,7 +2,7 @@ import type { GlobalConfig } from "semantic-release";
 
 const config: GlobalConfig = {
   tagFormat: "v${version}",
-  repositoryUrl: "github.com/524H0003/TDTU-auto-login",
+  repositoryUrl: "https://github.com/524H0003/TDTU-auto-login",
   branches: ["main", { name: "dev-*", prerelease: "dev" }],
   plugins: [
     "@semantic-release/commit-analyzer",
