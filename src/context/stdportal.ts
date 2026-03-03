aeadeb8e3ed6d18e3356c8de387a94f8503214e6
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-03 02:56:15 +0000

    fix: modified script for stdportal

--- src/context/stdportal.ts
+++ src/context/stdportal.ts
@@ -1,7 +1,30 @@
 import { execute } from ".";
 
-execute({
+execute<{ url: string }>({
   usernameField: "user",
   passwordField: "pass",
-  url: "https://stdportal.tdtu.edu.vn/Login/SignIn",
+  url: () =>
+    "https://stdportal.tdtu.edu.vn/Login/SignIn?ReturnURL=" +
+    window.location.origin +
+    window.location.pathname,
+  postFunc: async ({ url }) => {
+    window.location.href = url;
+  },
+  conditionFunc: async () => {
+    try {
+      const response = await fetch(
+        "https://stdportal.tdtu.edu.vn/Alert/ThongBaoNotification",
+        { method: "Post" },
+      );
+
+      if (!response.ok) return false;
+      else {
+        const data = await response.text();
+
+        return data !== "[]";
+      }
+    } catch {
+      return true;
+    }
+  },
 });
