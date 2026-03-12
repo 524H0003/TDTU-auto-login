044e7889d246f817a7197e6258b8665063cb4b2b
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-12 04:47:43 +0000

    feat(wifilib): init auto login

---
+++ src/context/wifilib.ts
@@ -0,0 +1,17 @@
+import { execute, getHiddenInput } from ".";
+
+execute({
+  usernameField: "username",
+  passwordField: "password",
+  url: location.origin,
+  postFunc() {
+    window.close();
+  },
+  isFormData: false,
+  extendFields(formData) {
+    formData.append("4Tredir", getHiddenInput('input[name="4Tredir"]'));
+    formData.append("magic", getHiddenInput('input[name="magic"]'));
+  },
+});
+
+export const runOnUpdate = true;
